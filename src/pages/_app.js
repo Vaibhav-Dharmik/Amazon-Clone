7115a25c3d862dd2a7b8614473de88a0d07068b0
vaibhav dharmik <vaibhav.crewbyte@gmail.com>
2021-05-22 14:35:09 +0530

    app build

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,13 @@
-import { Provider } from 'react-redux'
-import { store } from '../app/store'
-import '../styles/globals.css'
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
