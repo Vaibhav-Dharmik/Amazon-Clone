586e5b3fec637ac43d143d39180c8561989b1e87
Vaibhav Sanjayrao Dharmik <55710097+Vaibhav-Dharmik@users.noreply.github.com>
2021-05-26 15:35:05 +0530

    to string

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
   env: {
-    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+    stripe_public_key: `${process.env.STRIPE_PUBLIC_KEY}`,
   },
 };
