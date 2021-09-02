66f430893dd5644e934378c4006030b208e91af5
vaibhav dharmik <vaibhav.crewbyte@gmail.com>
2021-09-02 15:59:52 +0530

    leftSideMenu Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,31 @@ import { signIn, signOut, useSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import { selectItems } from "../slices/basketSlice";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { route } from "next/dist/server/router";
 
+import ClipboardListIcon from "@heroicons/react/outline/ClipboardListIcon";
+import QuestionMarkCircleIcon from "@heroicons/react/solid/QuestionMarkCircleIcon";
+import HomeIcon from "@heroicons/react/solid/HomeIcon";
+import LogoutIcon from "@heroicons/react/outline/LogoutIcon";
+import LibraryIcon from "@heroicons/react/outline/LibraryIcon";
+import ScaleIcon from "@heroicons/react/outline/ScaleIcon";
+import { CreditCardIcon, LockClosedIcon } from "@heroicons/react/solid";
+import MapIcon from "@heroicons/react/outline/MapIcon";
+import DocumentDuplicateIcon from "@heroicons/react/outline/DocumentDuplicateIcon";
+import UserIcon from "@heroicons/react/outline/UserIcon";
+import { XIcon } from "@heroicons/react/outline";
+
+const closeLeftSideMenuView = (e) => {
+  setLeftSideMenuView(false);
+};
 function Header() {
+  const [leftSideMenuView, setLeftSideMenuView] = useState(false);
+  const handleLeftSideMenuview = () => {
+    setLeftSideMenuView(!leftSideMenuView);
+    console.log(!leftSideMenuView);
+  };
+
   const [session] = useSession();
   const router = useRouter();
   const items = useSelector(selectItems);
@@ -71,7 +94,12 @@ function Header() {
       {/* Button */}
       <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
         <p className="link flex items-center">
-          <MenuIcon className="h-6 mr-1" />
+          <MenuIcon
+            className="h-6 mr-1"
+            onClick={(e) => {
+              handleLeftSideMenuview();
+            }}
+          />
           All
         </p>
         <p className="link">Prime Video</p>
@@ -84,6 +112,104 @@ function Header() {
         <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
         <p className="link hidden lg:inline-flex">Health & Personal Care</p>
       </div>
+      {leftSideMenuView && (
+        <div className="z-40">
+          {/* <LeftSideMenu /> */}
+          <div>
+            <div className="bg-amazon_blue-light leftSideMenu_container ml-0 flex flex-col justify-between  z-40 fixed h-screen top-0">
+              <div className="mx-4">
+                <div
+                  onClick={() => {
+                    handleLeftSideMenuview();
+                  }}
+                  className="cursor-pointer"
+                >
+                  <XIcon className="h-6 w-6 mr-0 leftSideMenu_icons text-white justify-items-end ml-auto" />
+                </div>
+                <div
+                  className="text-white flex link"
+                  onClick={() => {
+                    router.push("/");
+                    console.log("HOME");
+                  }}
+                >
+                  <HomeIcon className="h-6 w-6  leftSideMenu_icons" />
+                  <span className="ml-2 leftSideMenu_iconsTitle"> Home</span>
+                </div>
+
+                <div
+                  className="text-white flex link"
+                  onClick={() => {
+                    router.push("/Bookings", "/Bookings");
+                    console.log("BOOKINGS");
+                  }}
+                >
+                  <ClipboardListIcon className="h-6 w-6 leftSideMenu_icons" />
+                  <span className="ml-2 leftSideMenu_iconsTitle">
+                    {" "}
+                    BOOKINGS
+                  </span>
+                </div>
+                <div
+                  className="text-white flex link"
+                  onClick={() => {
+                    router.push("/Account", "/Account");
+                    console.log("ACCOUNT");
+                  }}
+                >
+                  <UserIcon className="h-6 w-6  leftSideMenu_icons" />
+                  <span className="ml-2 leftSideMenu_iconsTitle"> ACCOUNT</span>
+                </div>
+                <div
+                  className="text-white flex link"
+                  onClick={() => {
+                    router.push("/Addresses", "/Addresses");
+                    console.log("ADDRESSES");
+                  }}
+                >
+                  <MapIcon className="h-6 w-6  leftSideMenu_icons" />
+                  <span className="ml-2 leftSideMenu_iconsTitle">
+                    {" "}
+                    ADDRESSES
+                  </span>
+                </div>
+                <div
+                  className="text-white flex link"
+                  onClick={() => {
+                    router.push("/Wallate", "/Wallate");
+                    console.log("WALLATE");
+                  }}
+                >
+                  <CreditCardIcon className="h-6 w-6  leftSideMenu_icons" />
+                  <span className="ml-2 leftSideMenu_iconsTitle"> WALLATE</span>
+                </div>
+                <div
+                  className="text-white flex link"
+                  onClick={() => {
+                    router.push("/Help", "/Help");
+                    console.log("HELP");
+                  }}
+                >
+                  <QuestionMarkCircleIcon className="h-6 w-6  leftSideMenu_icons" />
+                  <span className="ml-2 leftSideMenu_iconsTitle"> HELP</span>
+                </div>
+                <hr />
+                <div
+                  className="text-white flex link"
+                  onClick={() => {
+                    console.log("LOGOUT");
+                  }}
+                >
+                  <LogoutIcon className="h-6 w-6  leftSideMenu_icons" />
+                  <span className="ml-2 leftSideMenu_iconsTitle"> LOGOUT</span>
+                </div>
+                <hr />
+              </div>
+              <div className="text-white">lower menu</div>
+            </div>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
